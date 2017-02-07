6916b6cf30eb03eedb23b034e3aa0219e15749d5
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-02-07 11:55:04 +0000

    Modify application routes

--- test/calculator/routes/RoutesSpec.scala
+++ test/calculator/routes/RoutesSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package calculator.routes
 
 import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
@@ -6,9 +22,8 @@ class RoutesSpec extends UnitSpec with WithFakeApplication {
 
   "The route for the welcome action on the home controller" should {
     "be /vat-flat-rate-calculator-frontend" in {
-      calculator.controllers.routes.HomeController.welcome().url shouldBe "/vat-flat-rate-calculator-frontend"
+      calculator.controllers.routes.HomeController.welcome().url shouldBe "/vat-flat-rate-calculator"
     }
   }
 
 }
-
