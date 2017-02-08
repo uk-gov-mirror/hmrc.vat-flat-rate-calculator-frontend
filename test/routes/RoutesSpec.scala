4b11c7a9d91006e117c27bb7b34aa2fd9f7ed9e6
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-02-08 16:35:47 +0000

    Fix unit test

--- test/routes/RoutesSpec.scala
+++ test/routes/RoutesSpec.scala
@@ -22,7 +22,7 @@ class RoutesSpec extends UnitSpec with WithFakeApplication {
 
   "The route for the welcome action on the home controller" should {
     "be /check-your-vat-flat-rate" in {
-      calculator.controllers.routes.HomeController.welcome().url shouldBe "/check-your-vat-flat-rate"
+      controllers.routes.HomeController.welcome().url shouldBe "/check-your-vat-flat-rate"
     }
   }
 
