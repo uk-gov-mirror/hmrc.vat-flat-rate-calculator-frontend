286b8c2179c44b9058e2f96b48be42e12fa5a944
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-02-19 17:23:16 +0000

    add result controller tests

--- test/routes/RoutesSpec.scala
+++ test/routes/RoutesSpec.scala
@@ -22,16 +22,46 @@ class RoutesSpec extends UnitSpec with WithFakeApplication {
   val baseUrl = "/check-your-vat-flat-rate"
 
   val vatReturnPeriod: String = baseUrl+"/vat-return-period"
+  val turnover: String = baseUrl+"/turnover"
+  val costOfGoods: String = baseUrl+"/cost-of-goods"
+  val result: String = baseUrl+"/vat-flat-rate-result"
 
   "The route for the vatReturnPeriod action on the vatReturnPeriod controller" should {
     "be /check-your-vat-flat-rate/vat-return-period" in {
       controllers.routes.VatReturnPeriodController.vatReturnPeriod().url shouldBe s"$vatReturnPeriod"
     }
-
+  }
     "The route for the submitVatReturnPeriod action on the vatReturnPeriod controller" should {
-      " be /check-your-vat-flat-rate/page-one" in {
+      " be /check-your-vat-flat-rate/vat-return-period" in {
         controllers.routes.VatReturnPeriodController.submitVatReturnPeriod().url shouldBe s"$vatReturnPeriod"
       }
+    }
+
+  "The route for the turnover action on the turnover controller" should {
+    " be /check-your-vat-flat-rate/turnover" in {
+      controllers.routes.TurnoverController.turnover().url shouldBe s"$turnover"
+    }
+  }
+
+  "The route for the submit action on the turnover controller" should {
+    " be /check-your-vat-flat-rate/turnover" in {
+      controllers.routes.TurnoverController.submitTurnover().url shouldBe s"$turnover"
+    }
+  }
+
+  "The route for the costOfGoods action on the costOfGoods controller" should {
+    "be /check-your-vat-flat-rate/cost-of-goods" in {
+      controllers.routes.CostOfGoodsController.costOfGoods().url shouldBe s"$costOfGoods"
+    }
+  }
+  "The route for the submit action on the costOfGoods controller" should {
+    "be /check-your-vat-flat-rate/cost-of-goods" in {
+      controllers.routes.CostOfGoodsController.submitCostOfGoods().url shouldBe s"$costOfGoods"
+    }
+  }
+  "The route for the result action on the result controller" should {
+    "be /check-your-vat-flat-rate/cost-of-goods" in {
+      controllers.routes.ResultController.result().url shouldBe s"$result"
     }
   }
 
