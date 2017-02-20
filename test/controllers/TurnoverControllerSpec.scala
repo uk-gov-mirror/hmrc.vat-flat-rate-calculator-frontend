931d405c87ffc792b1258aff3d6e9297c6e04df3
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-02-20 23:12:28 +0000

    add csrf token filter bypass

--- test/controllers/TurnoverControllerSpec.scala
+++ test/controllers/TurnoverControllerSpec.scala
@@ -14,45 +14,20 @@
  * limitations under the License.
  */
 
-/*
- * Copyright 2017 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package controllers
 
-import akka.stream.Materializer
-import config.AppConfig
-import controllers.predicates.ValidatedSession
-import forms.VatFlatRateForm
 import helpers.ControllerTestSpec
-import models.{ResultModel, VatFlatRateModel}
+import models.VatFlatRateModel
 import org.jsoup.Jsoup
 import org.mockito.ArgumentMatchers
 import org.mockito.Mockito.when
-import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.mock.MockitoSugar
-import org.scalatestplus.play.OneAppPerSuite
 import play.api.http.Status
+import play.api.i18n.Messages
 import play.api.i18n.Messages.Implicits._
-import play.api.i18n.{Messages, MessagesApi}
-import play.api.inject.Injector
 import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import services.StateService
 import uk.gov.hmrc.play.http.SessionKeys
-import uk.gov.hmrc.play.test.UnitSpec
 
 import scala.concurrent.Future
 
