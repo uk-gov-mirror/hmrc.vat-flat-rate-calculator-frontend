6a711105e54bdb33ab1720279b592116c6070c1d
samuel-raspin <samuel.raspin@digital.hmrc.gov.uk>
2017-02-23 16:07:01 +0000

    removed unused code

--- public/javascripts/vfr.js
+++ public/javascripts/vfr.js
@@ -3,9 +3,6 @@ $(document).ready($(function() {
     $('[dataMetrics]').each(function() {
         var metrics = $(this).attr('dataMetrics');
         var parts = metrics.split(':');
-        console.log("!!!!!!!!!!!!!!!!!!!!!!");
-        console.log("WORKING");
-        console.log("!!!!!!!!!!!!!!!!!!!!!!");
         ga('send', 'event', parts[0], parts[1], parts[2]);
     });
 
