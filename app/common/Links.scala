3ad6a821d3f638881603dc1b5d586c949ab32f0c
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-02-24 11:19:44 +0000

    change cost of goods content

--- app/common/Links.scala
+++ app/common/Links.scala
@@ -25,5 +25,6 @@ object Links {
   val flatRateScheme: String      = baseGovUrl + "vat-flat-rate-scheme/join-or-leave-the-scheme"
   val vatRegistration: String     = baseGovUrl + "vat-registration/cancel-registration"
   val howMuchYouPay: String       = baseGovUrl + "vat-flat-rate-scheme/how-much-you-pay"
+  val capitalExpenditureGoods: String = baseGovUrl + "government/publications/vat-notice-733-flat-rate-scheme-for-small-businesses/vat-notice-733-flat-rate-scheme-for-small-businesses#section15"
 
 }
