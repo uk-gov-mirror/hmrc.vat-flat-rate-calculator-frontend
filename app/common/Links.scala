ec79b3f610dcde1ede8a2435705d14adfecbf089
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-02-24 16:00:38 +0000

    final content changes

--- app/common/Links.scala
+++ app/common/Links.scala
@@ -21,7 +21,7 @@ object Links {
   final val baseGovUrl    = "http://www.gov.uk/"
   final val taxServiceUrl = "https://www.tax.service.gov.uk/"
 
-  val businessTaxAccount: String  = taxServiceUrl + "gg/sign-in?continue=https%3A%2F%2Fcustoms.hmrc.gov.uk%2Feprompt%2Fhttpssl%2FaddGeneralEmailAddress.do"
+  val businessTaxAccount: String  = taxServiceUrl + "business-account"
   val flatRateScheme: String      = baseGovUrl + "vat-flat-rate-scheme/join-or-leave-the-scheme"
   val vatRegistration: String     = baseGovUrl + "vat-registration/cancel-registration"
   val howMuchYouPay: String       = baseGovUrl + "vat-flat-rate-scheme/how-much-you-pay"
