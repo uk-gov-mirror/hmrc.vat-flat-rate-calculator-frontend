e9373fd0f59ec09a329cca47e4bdc2f240eefdef
MarkAKelly <mark.kelly@digital.hmrc.gov.uk>
2017-03-01 16:25:53 +0000

    revert libraries

--- project/FrontendBuild.scala
+++ project/FrontendBuild.scala
@@ -16,7 +16,7 @@ object FrontendBuild extends Build with MicroService {
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.13.0",
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.11.0",
     "uk.gov.hmrc" %% "play-partials" % "5.3.0",
     "uk.gov.hmrc" %% "play-authorised-frontend" % "6.3.0",
     "uk.gov.hmrc" %% "play-config" % "4.2.0",
